31f116274026fcd29757a77a3b8b71cc4b74254b
Ganesh Tiwari <ganesht049@gmail.com>
2022-11-12 19:14:52 +0530

    added links to events page

---
+++ src/pages/events/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+    return <div>
+        This is events page
+    </div>
+}
+
+export default Page;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import logo from '@site/static/img/logo.png'
+import { SocialIcon } from 'react-social-icons';
 
 import styles from './index.module.css';
 
@@ -13,9 +14,24 @@ export default function Home() {
     <div className={`${styles.flex} ${styles.column} ${styles.mc} ${styles.cc} ${styles.full_page}`}>
       <img src={logo}/>
 
-      <h1 className={`${styles.topPadding5rem}`}>
-        Chainlink Smart Contracts - Mumbai Chapter
+      <h1 className={`${styles.topPadding5rem} text-align-center`}>
+        Chainlink Mumbai - Connected Smart Contracts
       </h1>
+
+      <div className={`${styles.flex} ${styles.gap2} ${styles.topPadding2rem}`}>
+        <SocialIcon 
+          url='https://twitter.com/ChainlinkMumTwt' 
+          target='_blank'/>
+
+        <SocialIcon 
+          url='https://www.meetup.com/chainlink-mumbai' 
+          target='_blank'/>
+      </div>
+
+      <Link href='/events/' className={`text-white ${styles.topPadding2rem} underline`}>
+        Check out our events here
+      </Link>
+      
     </div>
   );
 }
